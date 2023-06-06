15dde7bc7460f26c1f22f6a6a21422456c627768
GUSTAVOSOLIVEIRA07 <111474763+GUSTAVOSOLIVEIRA07@users.noreply.github.com>
2023-06-06 18:04:10 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/intransit/MainActivity.java
+++ app/src/main/java/com/example/intransit/MainActivity.java
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
+  
     public void sendMessage(View view) {
-        // Do something in response to button
+     
     }
 }
